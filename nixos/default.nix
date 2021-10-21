399da085b1e585acc3558affd22fc4e7438328b5
mliu-mettle <michael.liu@mettle.co.uk>
2021-10-21 20:57:41 +0100

    Update default value.

--- nixos/default.nix
+++ nixos/default.nix
@@ -6,7 +6,7 @@
   work = {
     name          = "mliu-mettle";
     email         = "michael.liu@mettle.co.uk";
-    signing.key   = "5DD715C45B3E5F63";
+    sign          = "5DD715C45B3E5F63";
     gpg           = "3d7b971619dbc75e448938174f9b6a131bf4587e";
     passwordstore = {
       git = "git@github.com:mliu-mettle/password-store.git";
