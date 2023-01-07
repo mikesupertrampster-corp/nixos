6d169827d9849ad5b1c768fa6702a42cecf394fb
michael.liu <mikesupertrampster@gmail.com>
2023-01-07 18:03:54 +0700

    Update for 2023.

--- nixos/nixos-one/system.nix
+++ nixos/nixos-one/system.nix
@@ -1,32 +1,15 @@
 { config, pkgs, ... }:
 
-let
-  default = import (pkgs.fetchFromGitHub {
-     owner  = "mikesupertrampster";
-     repo   = "nixos";
-     rev    = "033c3863e729a51749949ad3c688bc51f1a2b45b";
-     sha256 = "sha256:HmC7ypvTNT1wQYYw6ggQAa30RqynpP3S0/STFUhEEbQ=";
-   });
-in
 {
-  boot = {
-    loader = {
-      efi.canTouchEfiVariables = true;
-    };
-  };
-
   services = {
     acpid.enable = true;
     xserver = {
       dpi = 160;
       videoDrivers = [ "nvidia" ];
       windowManager.i3.extraSessionCommands = "xrandr "
-      + "--output DP-2   --mode 2560x1440 --pos 0x0 "
-      + "--output HDMI-0 --mode 2560x1440 --pos 2560x0 "
-      + "--output DP-4   --mode 2560x1440 --pos 5120x0";
+      + "--output HDMI-0 --mode 1920x1080 --pos 3840x0 --primary";
     };
   };
 
-  i18n.defaultLocale = default.locale.i18n;
   networking.hostName = "nixos-one";
 }
